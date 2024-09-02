b68ef440570313cf46bea22b9a24f0a5da39d023
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2024-09-02 17:30:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,10 @@ SESSION3 = getenv("SESSION3", "")
 SESSION4 = getenv("SESSION4", "")
 SESSION5 = getenv("SESSION5", "")
 SESSION6 = getenv("SESSION6", "")
+SESSION7 = getenv("SESSION7", "")
+SESSION8 = getenv("SESSION8", "")
+SESSION9 = getenv("SESSION9", "")
+SESSION10 = getenv("SESSION10", "")
 
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 MONGO_URL = os.environ["MONGO_URL"]
