03f923a007f846218c5cbd0c00744c62e61ad4e6
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2024-09-12 01:17:02 +0530

    Update p1.py

--- Akeno/plugins/p1.py
+++ Akeno/plugins/p1.py
@@ -9,12 +9,12 @@ TARGET_CHAT_ID = [-1002068064532, -1002214107507]
 TARGET_CHANNEL_ID = [-1002195132204, -1002212156629]
 client_reply_messages = {
     "two": "UQAXRBHAaTZcyRgVN54K8FqnAS9ZnhLT8NOhV36EHUtrpCQi",
-    "one": "UQA4Q17hbb8qBtkjso0wOty3SBh2krMdLZi_HIVDSQHlHdpq",#"UQAcpGyz8ciRwYMBQIwT7cHrrmaPgAKNMF95PG-qaPc-hpEA",
+    "one": "UQA4Q17hbb8qBtkjso0wOty3SBh2krMdLZi_HIVDSQHlHdpq",
     "three": "UQBqAubq2phpLyOXyQT4twxryLsWZFIJ437gwLhvohAc18kq",
     "four": "UQDoiTdMCqHvV93tGiCeHW4s5ULnFPnjpkC0tZbOyi_IAEJR",
     "five": "UQDVAvu6YJcm5E90OGD6FGaqmakDCS8QdMeJXW13DsOFdNsH",
     "six": "UQA89uwzmb0kjzzQunF-NH0G7pQvVj2k5awqWduQKOyTiw96",
-    "sev": "UQDmw_yd95yWYjFnPnHZgFmgfslqNjI9qUp3_NTN_KuX0fSi",
+    "sev": "UQAcpGyz8ciRwYMBQIwT7cHrrmaPgAKNMF95PG-qaPc-hpEA", #"UQDmw_yd95yWYjFnPnHZgFmgfslqNjI9qUp3_NTN_KuX0fSi",
     "eig": "UQDkUJr4Otn4is--C26zmhvg7mEWRooHv7urvom93gBrZ_80",
     "nin": "", 
     "ten": ""   
