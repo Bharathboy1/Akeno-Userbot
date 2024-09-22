ae30a3d7427dbe0d39057e275d88e7cbda14d045
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2024-09-22 10:31:25 +0530

    /

--- Akeno/__main__.py
+++ Akeno/__main__.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright 2020-2023 (c) Randy W @xtdevs, @xtsea
-#
-# from : https://github.com/TeamKillerX
-# Channel : @RendyProjects
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
 import importlib
@@ -60,11 +43,7 @@ async def main():
             LOGS.info(f"Started {ex.first_name}")
             await cli.send_message("me", "Starting Akeno Userbot")
 
-            # Attempt to join chat, handle blocked user
-            try:
-                await cli.join_chat("RendyProjects")
-            except UserIsBlocked:
-                LOGS.info("You have been blocked. Please support @xtdevs")
+            
 
         # Wait for all events to be handled
         await idle()
